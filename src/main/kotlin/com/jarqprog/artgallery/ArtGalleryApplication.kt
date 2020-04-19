dcf19e2891525416c11901fdbe89cee09e8c3092
jarqprog <jaroslaw.kucharczyk@tesco.com>
2020-04-19 09:49:17 +0200

    initial commit

---
+++ src/main/kotlin/com/jarqprog/artgallery/ArtGalleryApplication.kt
@@ -0,0 +1,11 @@
+package com.jarqprog.artgallery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtGalleryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtGalleryApplication>(*args)
+}
